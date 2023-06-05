11f34f5e7b8188712c4e41fbc17194ecf24522f5
MohanRamaera <mohan.it@ramaera.in>
2023-06-05 11:35:49 +0530

    package and login middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,14 @@
 import { GraphQLModule } from '@nestjs/graphql';
 import { Logger, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { PrismaModule, loggingMiddleware } from 'nestjs-prisma';
+import { PrismaModule } from 'nestjs-prisma';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppResolver } from './app.resolver';
 import { AuthModule } from 'src/auth/auth.module';
 import { UsersModule } from 'src/users/users.module';
 import config from 'src/common/configs/config';
+import { loggingMiddleware } from 'src/common/middleware/logging.middleware';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { GqlConfigService } from './gql-config.service';
 import { CustomersModule } from './customers/customers.module';
